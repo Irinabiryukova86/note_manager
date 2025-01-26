d14f4937a861929ad61e6a89a6bc5e29512bab6b
Irinabiryukova86 <aic1986@mail.ru>
2025-01-26 09:40:21 +0300

    Add files via upload

---
+++ greetengs.py
@@ -0,0 +1,12 @@
+username = 'Irina'
+print('Имя пользователя:', username)
+title = 'title'
+print('Заголовок заметки:',title)
+content = 'content'
+print('Описание заметки:', content)
+status = 'status'
+print('Статус заметки:',status)
+created_date = '26-01-2025'
+print('created_date:', created_date)
+issue_date = '26-02-2025'
+print('issue_date:', issue_date)
