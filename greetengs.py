fd5c6767a56f42bfc098a960d9ab9552322c2927
Irinabiryukova86 <aic1986@mail.ru>
2025-01-28 09:40:42 +0300

    Update greetengs.py

--- greetengs.py
+++ greetengs.py
@@ -1,12 +1,12 @@
 username = 'Irina'
 print('Имя пользователя:', username)
 title = 'title'
-print('Заголовок заметки:',title)
+print('Заголовок заметки:', title)
 content = 'content'
 print('Описание заметки:', content)
 status = 'status'
-print('Статус заметки:',status)
+print('Статус заметки:', status)
 created_date = '26-01-2025'
-print('created_date:', created_date)
+print('Дата создания заметки:', created_date)
 issue_date = '26-02-2025'
-print('issue_date:', issue_date)
+print('Дата истечения заметки:', issue_date)
