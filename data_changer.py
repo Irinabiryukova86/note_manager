deba509ede2086573618f7ab924adf431b7775b8
Irinabiryukova86 <aic1986@mail.ru>
2025-01-28 15:45:56 +0300

    Update data_changer.py

--- data_changer.py
+++ data_changer.py
@@ -1,4 +1,4 @@
 created_date = '26-01-2025'
-print('created_date:', created_date[0:5])
+print('Дата создания заметки:', created_date[0:5])
 issue_date = '26-02-2025'
-print('issue_date:', issue_date[0:5])
+print('Дата истечения заметки:', issue_date[0:5])
